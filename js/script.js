9bd9e91bd6cce239c8a95187992b76154696f096
isaias lima <izauiaslima@gmail.com>
2023-10-06 21:55:52 -0300

    Meu primeiro commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let n1; 
+let n2;
+
+
+//n2 = parseInt(prompt("qual a sua idade pivete?"));
+
+//if( n2 >= "18") 
+{
+ //  alert("acesso liberado")
+}
+//else
+{
+  //  alert("acesso, negado")
+   // window.location.href =  "https://www.google.com/search?client=opera-gx&hs=k8T&sca_esv=560260547&sxsrf=AB5stBh5hyxYhC10zvbQckI8EO8CD6OvNQ:1693022631133&q=não+pode&tbm=isch&source=lnms&sa=X&ved=2ahUKEwiC15nkuPmAAxVGppUCHfn-CRcQ0pQJegQIDhAB&biw=1313&bih=656&dpr=1#imgrc=IPRdecJa7cNTFM"
+}
+const nome = document.getElementById('')
+    
+
+
+
+ 
